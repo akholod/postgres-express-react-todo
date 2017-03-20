65b333dcbaebabfc8aa3193026fc9c7d726f5590
akholod <andrey.kokhanevich@gmail.com>
2017-03-20 14:17:12 +0200

    init repo

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('*', (req, res) => res.status(200).send({
+    message: 'test message',
+}));
+
+app.listen(port, () => {
+    global.console.log(`App listen on port ${port}`);
+});
+
+module.exports = app;
