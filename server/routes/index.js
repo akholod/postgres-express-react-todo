3150de09f5fd73c39a7716258ecba8721255d674
akholod <andrey.kokhanevich@gmail.com>
2017-05-14 19:05:14 +0300

    todoitem controller imlemented

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ const todosController = require('../controllers').todos;
 router.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
 }))
-.post('/api/todos', todosController.create);
+.post('/api/todos', todosController.create)
+.get('/api/todos', todosController.list);
 
 module.exports = router;
