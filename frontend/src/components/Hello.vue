e332d78b7a00aa91317130199c78f88d1516cb78
akholod <andrey.kokhanevich@gmail.com>
2017-08-03 00:37:03 +0300

    vue and vuex added

---
+++ frontend/src/components/Hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ count }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  computed: {
+    count() {
+      return this.$store.getters.count;
+    },
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
